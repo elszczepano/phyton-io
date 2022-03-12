3aac0bb494f6f410269e71f186f776e889755027
Dominik <szczepaniak.dominik98@gmail.com>
2022-03-12 14:19:22 +0100

    Add hello and dodaj functions

---
+++ test.py
@@ -0,0 +1,13 @@
+# Simple calc
+
+def hello(name):
+    return "Hello" + str(name)
+
+def dodaj(a,b):
+    wynik = float(a) + float(b)
+    return wynik;
+
+pierwsza = input()
+druga = input()
+
+print(dodaj(pierwsza,druga))
